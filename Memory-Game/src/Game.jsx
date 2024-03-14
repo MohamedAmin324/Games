04a96764a78cddef1b1504f1c5e790a1ff4c7c13
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2024-03-14 21:36:44 +0100

    Add basic styles

--- Memory-Game/src/Game.jsx
+++ Memory-Game/src/Game.jsx
@@ -1,5 +1,16 @@
+import { useState } from 'react';
 import { generateGameList } from './data';
 
 export default function Game() {
-	return <button onClick={() => console.log(generateGameList())}>click</button>;
+	const [list, setList] = useState(generateGameList);
+
+	return (
+		<>
+			{list.map((img, index) => (
+				<div className='hidden' key={index}>
+					<img width='80px' src={img} />
+				</div>
+			))}
+		</>
+	);
 }
--- Memory-Game/src/main.jsx
+++ Memory-Game/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Settings from './Game.jsx';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
